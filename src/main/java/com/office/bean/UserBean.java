1a0534a1311c4a6b6792d5b2f6e31d3704ba86e7
Deepak Rohan Sekar <deepakrohan@hotmail.com>
2016-10-23 19:33:02 -0400

    initial

---
+++ src/main/java/com/office/bean/UserBean.java
@@ -0,0 +1,31 @@
+package com.office.bean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name="user")
+@SessionScoped
+public class UserBean implements Serializable {
+	
+	private String name,email;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
+}
---
+++ src/main/java/com/office/controller/IndexCtrl.java
@@ -0,0 +1,5 @@
+package com.office.controller;
+
+public class IndexCtrl {
+
+}
